e1ad805d75f082bbe9479ea9295e20821f1e9eb2
Eduardo Pitty Lee <Creative507@gmail.com>
2023-11-04 07:27:56 -0500

    Removing and adding some comments.

--- shipping.py
+++ shipping.py
@@ -10,9 +10,9 @@ elif weight > 6 and weight <= 10:
 else:
   cost_ground = weight * 4.75 + 20.00
 #print(cost_ground)
-#Premium shipping
+
+#Premium ground shipping
 cost_premium = 125.00
-#print(cost_premium)
 
 #Drone shipping
 if weight <= 2:
@@ -23,7 +23,6 @@ elif weight > 6 and weight <= 10:
   cost_drone = weight * 12.00
 else:
   cost_drone = weight * 14.25
-#print(cost_drone)
 
 #Cheapest method
 if cost_ground < cost_premium and cost_ground < cost_drone:
